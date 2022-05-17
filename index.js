31a9ed583207c105ae0d3d431008baf9d9e1cf31
mdhainf20 <mdhanif115825@gmail.com>
2022-05-18 01:28:05 +0600

    added new

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.qytn8.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-const serviceAccount = require('./service-related-project-firebase-adminsdk.json')
+const serviceAccount = require('./service-related-project-firebase-adminsdk.json') 
 // const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT); 
 
 admin.initializeApp({
